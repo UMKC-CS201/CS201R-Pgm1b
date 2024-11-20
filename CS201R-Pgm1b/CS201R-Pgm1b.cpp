3c4ff4c74774dc7c8b88b9015e459ffd396667c4
Joan Gladbach <jjgr29@umsystem.edu>
2024-11-20 12:56:36 -0600

    Add project files.

---
+++ CS201R-Pgm1b/CS201R-Pgm1b.cpp
@@ -0,0 +1,21 @@
+// CS201R-Pgm1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x1, x2, y1, y2;
+	
+	//generate 4 random values from -10 to 10 for each value
+	
+
+
+	//calculate the distance between the points (x1,y1) and (x2,y2)
+	 
+
+
+	//print the distance between these points
+	cout << "The distance between the points is: " << endl;
+}
